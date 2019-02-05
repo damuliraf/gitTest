fd1c524e3e6469877e2ded9ca9a95eb5e065c7c4
Ddamulira Francis <damulirafrancis@gmail.com>
2019-02-05 08:56:22 +0300

    Update Summative1.py

--- Summative1.py
+++ Summative1.py
@@ -10,7 +10,7 @@ def generate_list(array1):
     # first loop
     for i in range(len(array1) + 1):
         # second loop
-        for j in range(i+1, len(array1) + 1):
+        for j in range(i, len(array1) + 1):
             # call function to generate random numbers between 0 and 1 and append to your list
             sub = generate_floats(0,1)
             sublist.append(sub)
@@ -31,7 +31,7 @@ def generate_floats(low, high):
         result.append(x)
     return result
 
-def write_to_file(dataframe,filename):
+def write_to_file(dataframe,filename,times):
     if os.path.exists(filename):
         append_write = 'a'  # append if already exists
     else:
@@ -39,24 +39,24 @@ def write_to_file(dataframe,filename):
     print(append_write)
     highscore = csv.writer(open(filename, append_write))
     for key,val in dataframe.items():
-        highscore.writerow([key,val])
+        highscore.writerow([key,val,times])
 
 
 
 if __name__ == "__main__":
 # driver code
 # generate sequential range of object to hold data
- region = list(range(1,32))
+ region = list(range(1,33))
 # generate 16 float readings for each of the object between 0 and 1
  sensor_readings = generate_list(region)
- #print(region)
+ print(region)
  # generate dictionary to combine the two lists with region objects being the key and sensor readings as the values
  sensor_data = dict(zip(region, sensor_readings))
  print(sensor_data)
  # time stamp
  now = datetime.now()
  print("timestamp =", now)
- write_to_file(sensor_data,inputfile)
+ write_to_file(sensor_data,inputfile,now)
 
 
 
